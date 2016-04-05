89c293cfa8030282fd3ea45f0377af0f905f6bea
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-04-05 10:31:33 -0700

    Add placeholder test just so that we can set up Travis.

---
+++ tests/testBootstrap.php
@@ -0,0 +1,11 @@
+<?php
+namespace Consolidation\Bootstrap;
+
+class BootstrapTests extends \PHPUnit_Framework_TestCase
+{
+    function testVolkswagonStyle()
+    {
+        $this->assertTrue(true);
+    }
+}
+
